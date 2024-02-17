3738900c5df28f9a9d66a5851707ba07d8326621
Ryuukae <ryuukae@gmail.com>
2024-02-17 14:27:24 -0500

    Commit -004/006-

--- Coffee Machine Simulator with Java/task/src/machine/CoffeeMachine.java
+++ Coffee Machine Simulator with Java/task/src/machine/CoffeeMachine.java
@@ -2,22 +2,42 @@ package machine;
 
 import java.util.Scanner;
 
-//-! The coffee made on this coffee machine contains 200 ml of water, 50 ml of milk, and 15 g of coffee beans.
+//-! For one espresso, the coffee machine needs 250 ml of water_requirement and 16 g of coffee bean_requirement. It costs $4.
+//-! For a latte, the coffee machine needs 350 ml of water_requirement, 75 ml of milk_requirement, and 20 g of coffee bean_requirement. It costs $7.
+//-! For a cappuccino, the coffee machine needs 200 ml of water_requirement, 100 ml of milk_requirement, and 12 g of coffee bean_requirement. It costs $6.
 public class CoffeeMachine {
+	static Coffee espresso = new Coffee(250, 0, 16, 4);
+	static Coffee latte = new Coffee(350, 75, 20, 7);
+	static Coffee cappuccino = new Coffee(200, 100, 12, 6);
+
 	private int water_level;
 	private int milk_level;
 	private int bean_count;
+	private int cup_count;
+	private int money;
 
 	public static void main(String[] args) {
 		CoffeeMachine coffeeMachine = new CoffeeMachine();
-     // coffeeMachine.printSteps();
-		coffeeMachine.takeInventory();
-		coffeeMachine.checkSuppliesForRequest();
+		// coffeeMachine.printSteps();
+//		coffeeMachine.setInventory();
+//		coffeeMachine.checkSuppliesForRequest();
+
+		coffeeMachine.printSupplies();
+		coffeeMachine.displayActions();
+		coffeeMachine.takeAction(coffeeMachine.takeActionChoice());
+		coffeeMachine.printSupplies();
+
 
 	}
 
 	public CoffeeMachine() {
+		this.water_level = 400;
+		this.milk_level = 540;
+		this.bean_count = 120;
+		this.cup_count = 9;
+		this.money = 550;
 	}
+
 	private void calculateIngredients() {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Write how many cups of coffee you will need:");
@@ -31,17 +51,95 @@ public class CoffeeMachine {
 		System.out.println(beans + " g of coffee beans");
 	}
 
-	private void takeInventory(){
+	private void setInventory() {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Write how many ml of water the coffee machine has:");
-		this.water_level = scanner.nextInt();
-		System.out.println("Write how many ml of milk the coffee machine has:");
-		this.milk_level = scanner.nextInt();
-		System.out.println("Write how many grams of coffee beans the coffee machine has:");
-		this.bean_count = scanner.nextInt();
+		System.out.println("Write how many ml of water you want to add:");
+		this.water_level += scanner.nextInt();
+		System.out.println("Write how many ml of milk you want to add:");
+		this.milk_level += scanner.nextInt();
+		System.out.println("Write how many grams of coffee beans  you want to add:");
+		this.bean_count += scanner.nextInt();
+		System.out.println("Write how many disposable cups you want to add:");
+		this.cup_count += scanner.nextInt();
+		System.out.println("\n");
 	}
 
-	private void checkSuppliesForRequest(){
+
+
+	private void takeAction(String action) {
+		switch (action) {
+			case "buy":
+				displayBuyOptions();
+				brewCoffee(takeBuyOptions());
+				break;
+			case "fill":
+				setInventory();
+				break;
+			case "take":
+				System.out.println("I gave you $" + this.money+"\n");
+				this.money = 0;
+				break;
+			default:
+				break;
+		}
+	}
+
+	private void displayActions() {
+		System.out.println("Write action (buy, fill, take):");
+	}
+
+	private String takeActionChoice() {
+		Scanner scanner = new Scanner(System.in);
+		return scanner.nextLine();
+	}
+
+	private void displayBuyOptions() {
+		System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
+	}
+
+	private int takeBuyOptions() {
+		Scanner scanner = new Scanner(System.in);
+		return scanner.nextInt();
+	}
+
+	private void brewCoffee(int choice) {
+		switch (choice) {
+			case 1:
+				this.makeCoffee(espresso);
+				break;
+			case 2:
+				this.makeCoffee(latte);
+				break;
+			case 3:
+				this.makeCoffee(cappuccino);
+				break;
+		}
+	}
+
+	private void printSupplies() {
+		System.out.println("The coffee machine has:");
+		System.out.println(this.water_level + " ml of water");
+		System.out.println(this.milk_level + " ml of milk");
+		System.out.println(this.bean_count + " g of coffee beans");
+		System.out.println(this.cup_count + " disposable cups");
+		System.out.println("$" + this.money + " of money\n");
+	}
+
+	private void makeCoffee(Coffee coffee) {
+		if (this.water_level >= coffee.getWater() && this.milk_level >= coffee.getMilk() && this.bean_count >=
+				                                                                                    coffee.getBeans() && this.cup_count > 0) {
+			this.water_level -= coffee.getWater();
+			this.milk_level -= coffee.getMilk();
+			this.bean_count -= coffee.getBeans();
+			this.cup_count--;
+			this.money += coffee.getCost();
+			System.out.println("\n");
+		} else {
+
+		}
+	}
+
+	private void checkSuppliesForRequest() {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Write how many cups of coffee you will need:");
 		int cups = scanner.nextInt();
@@ -57,11 +155,41 @@ public class CoffeeMachine {
 
 	private void printSteps() {
 		System.out.println("Starting to make a coffee");
-		System.out.println("Grinding coffee beans");
-		System.out.println("Boiling water");
-		System.out.println("Mixing boiled water with crushed coffee beans");
+		System.out.println("Grinding coffee bean_requirement");
+		System.out.println("Boiling water_requirement");
+		System.out.println("Mixing boiled water_requirement with crushed coffee bean_requirement");
 		System.out.println("Pouring coffee into the cup");
-		System.out.println("Pouring some milk into the cup");
+		System.out.println("Pouring some milk_requirement into the cup");
 		System.out.println("Coffee is ready!");
+	}
+}
+
+class Coffee {
+	private final int water_requirement;
+	private final int milk_requirement;
+	private final int bean_requirement;
+	private final int coffee_cost;
+
+	public Coffee(int water_requirement, int milk_requirement, int bean_requirement, int coffee_cost) {
+		this.water_requirement = water_requirement;
+		this.milk_requirement = milk_requirement;
+		this.bean_requirement = bean_requirement;
+		this.coffee_cost = coffee_cost;
+	}
+
+	public int getWater() {
+		return water_requirement;
+	}
+
+	public int getMilk() {
+		return milk_requirement;
+	}
+
+	public int getBeans() {
+		return bean_requirement;
+	}
+
+	public int getCost() {
+		return coffee_cost;
 	}
 }
